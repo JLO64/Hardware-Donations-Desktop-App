08eef6c3b66c97b97a98ccd346531e509dc670e9
64julianlopez@gmail.com <64julianlopez@gmail.com>
2019-11-24 10:00:54 -0800

    Added list of options

--- ARK-USB.py
+++ ARK-USB.py
@@ -1,7 +1,22 @@
-import os, sys
+import os, sys, glob
 sys.path.append("Python_Functions")
 import terminalColor
 
-terminalColor.printGreenString("STARTING PROGRAM")
+terminalColor.printGreenString("STARTING PROGRAM...\n")
+#os.system("./ARK-OS_Installer")
 
-os.system("./ARK-OS_Installer")
+#list files
+#print(glob.glob("ARK-OS/*"))
+
+
+print("What do you want to do?")
+intDecision = 0
+while ( (intDecision < 1) or (intDecision > 4) ):
+    try:
+        terminalColor.printBlueString("1. Flash a hard drive\n2. Upgrade a system with ARK-OS\n3. Download files\n4. Exit")
+        intDecision = int(input())
+        if ( (intDecision < 1) or (intDecision > 4) ):
+            terminalColor.printRedString("Invalid Input")
+    except:
+        intDecision = 0
+        terminalColor.printRedString("Invalid Input")
--- Python_Functions/terminalColor.py
+++ Python_Functions/terminalColor.py
@@ -1,14 +1,18 @@
 class bcolors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    BLUE = '\033[1;34m'
+    GREEN = '\033[1;32m'
+    RED = '\033[1;31m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
 def printRedString( stringToPrint ):
-    print( bcolors.WARNING + stringToPrint + bcolors.ENDC )
+    print( bcolors.RED + stringToPrint + bcolors.ENDC )
+
 def printGreenString( stringToPrint ):
-    print( bcolors.OKGREEN + stringToPrint + bcolors.ENDC )
+    print( bcolors.GREEN + stringToPrint + bcolors.ENDC )
+
+def printBlueString( stringToPrint ):
+    print( bcolors.BLUE + stringToPrint + bcolors.ENDC )
