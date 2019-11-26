794319945f6c415cbe6a5e31d90e80c59f53ece0
JLO64 <64julianlopez@gmail.com>
2019-11-26 13:25:03 -0800

    Started work on reading JSON

--- Python_Functions/downloadFiles.py
+++ Python_Functions/downloadFiles.py
@@ -1,13 +1,20 @@
-import urllib.request, os, sys
+import urllib.request, os, sys, json
 import terminalColor, fileFunctions
 
 def downloadFilesMain():
     downloadFilesList()
+    readFileList()
 
 def downloadFilesList():
     fileFunctions.checkForDirectory( os.path.expanduser('~') + "/HardwareDonations/Download_Links" )
 
     print("Downloading list of files")
-    url = "https://hardware-donations-database-gamma.s3-us-west-1.amazonaws.com/Misc_Items/Hardware_Donations_Account_EULA.txt"
-    urllib.request.urlretrieve(url, os.path.expanduser('~') + "/HardwareDonations/Download_Links/downloadsList.txt" )
+    url = "https://hardware-donations-database-gamma.s3-us-west-1.amazonaws.com/Misc_Items/DownloadList.txt"
+    urllib.request.urlretrieve(url, os.path.expanduser('~') + "/HardwareDonations/Download_Links/DownloadList.txt" )
     terminalColor.printGreenString("Download Finished")
+
+def readFileList():
+    with open(os.path.expanduser('~') + "/HardwareDonations/Download_Links/DownloadList.txt", 'r') as myfile:
+        data=myfile.read()
+    obj = json.loads(data)
+    print("usd: " + str(obj['name']))
