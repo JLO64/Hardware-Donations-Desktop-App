56b1ebdd3054faf9f7ea3c5285d7d1db7c0da2f5
64julianlopez@gmail.com <64julianlopez@gmail.com>
2019-11-26 20:16:24 -0800

    JSON reading works, working on how to cancel

--- Python_Functions/downloadFiles.py
+++ Python_Functions/downloadFiles.py
@@ -1,5 +1,6 @@
 import urllib.request, os, sys, json
 import terminalColor, fileFunctions
+import array as arr
 
 def downloadFilesMain():
     downloadFilesList()
@@ -17,4 +18,35 @@ def readFileList():
     with open(os.path.expanduser('~') + "/HardwareDonations/Download_Links/DownloadList.txt", 'r') as myfile:
         data=myfile.read()
     obj = json.loads(data)
-    print("usd: " + str(obj['name']))
+
+    intDecision = 0
+    terminateLoop = False
+    while ( ( (intDecision < 1) or (intDecision > len(downloadURLs)) ) or (terminateLoop == False) ):
+        
+        downloadURLs = []
+        for downloadable in obj["Legal"]:
+            downloadURLs.append(str(downloadable["url"]))
+            terminalColor.printBlueString( str(len(downloadURLs)) + ". [Legal]" + downloadable["name"] )
+            print("     Description: " + downloadable["description"] )
+            print("     Size: " + downloadable["size"] )
+        for downloadable in obj["PDFs"]:
+            downloadURLs.append(str(downloadable["url"]))
+            terminalColor.printBlueString( str(len(downloadURLs)) + ". [PDF]" + downloadable["name"] )
+            print("     Description: " + downloadable["description"] )
+            print("     Size: " + downloadable["size"] )
+        terminalColor.printBlueString( str(len(downloadURLs) + 1 ) + ". Exit" )
+
+        try:
+            print("Which file do you want to download?")
+            intDecision = int(input())
+            if ( (intDecision < 1) or (intDecision > ( len(downloadURLs) + 1 ) ) ):
+                terminalColor.printRedString("Invalid Input")
+            elif (intDecision == ( len(downloadURLs) ) ):
+                terminateLoop = True
+            else:
+                print("Downloading URL: " + downloadURLs[intDecision - 1])
+                terminateLoop = True
+        except:
+            intDecision = 0
+            terminalColor.printRedString("Invalid Input")
+    
--- Python_Functions/fileFunctions.py
+++ Python_Functions/fileFunctions.py
@@ -1,5 +1,5 @@
 import sys, os
 
-def checkForDirectory(programPath): #checks for "Hardware_Donations" directory and creates it if not found
+def checkForDirectory(programPath): #checks for "programPath" directory and creates it if not found
     if not os.path.exists(programPath):
         os.makedirs(programPath)
