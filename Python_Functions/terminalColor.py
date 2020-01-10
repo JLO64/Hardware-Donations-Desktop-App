fd3d16aa3f2b0d0be6e35b2b71de071afcf29d63
64julianlopez@gmail.com <64julianlopez@gmail.com>
2020-01-09 22:11:20 -0800

    Added all fields except comments to unit info page and added color

--- Python_Functions/terminalColor.py
+++ Python_Functions/terminalColor.py
@@ -3,6 +3,8 @@ import settingsJson
 class bcolors:
     HEADER = '\033[95m'
     BLUE = '\033[1;34m'
+    CYAN = '\033[0;36m'
+    YELLOW = '\033[0;33m'
     GREEN = '\033[1;32m'
     RED = '\033[1;31m'
     FAIL = '\033[91m'
@@ -21,3 +23,27 @@ def printGreenString( stringToPrint ):
 def printBlueString( stringToPrint ):
     if settingsJson.colorMode == True: print( bcolors.BLUE + stringToPrint + bcolors.ENDC )
     else: print(stringToPrint)
+
+def printCyanString( stringToPrint ):
+    if settingsJson.colorMode == True: print( bcolors.CYAN + stringToPrint + bcolors.ENDC )
+    else: print(stringToPrint)
+
+def generateRedString( stringToPrint ):
+    if settingsJson.colorMode == True: return bcolors.RED + stringToPrint.upper() + bcolors.ENDC 
+    else: return(stringToPrint.upper())
+
+def generateGreenString( stringToPrint ):
+    if settingsJson.colorMode == True: return( bcolors.GREEN + stringToPrint.upper() + bcolors.ENDC )
+    else: return(stringToPrint.upper())
+
+def generateBlueString( stringToPrint ):
+    if settingsJson.colorMode == True: return( bcolors.BLUE + stringToPrint + bcolors.ENDC )
+    else: return(stringToPrint)
+
+def generateCyanString( stringToPrint ):
+    if settingsJson.colorMode == True: return( bcolors.CYAN + stringToPrint + bcolors.ENDC )
+    else: return(stringToPrint)
+
+def generateYellowString( stringToPrint ):
+    if settingsJson.colorMode == True: return( bcolors.YELLOW + stringToPrint + bcolors.ENDC )
+    else: return(stringToPrint)
